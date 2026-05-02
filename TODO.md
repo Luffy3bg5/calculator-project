70221225fd3373758e1f452339b63fe3a0f1fb16
Rohan <luffy7744.op@gmail.com>
2026-05-03 02:45:03 +0530

    addded  a  todo  file and  the  checkboxes  in it

---
+++ TODO.md
@@ -0,0 +1,6 @@
+#My  tasks 
+to tick  down  the  checkboxes  use  crtl + shift + v 
+
+- [x] do  this 
+- [x] 2nd  task 
+- [x] 3rd  task 
--- index.js
+++ index.js
@@ -7,7 +7,6 @@ let  num1 , num2 ;
 let  inp  =  document.querySelector('.inp') ;
 document.querySelectorAll('.num').forEach(button => button.addEventListener('click',(e)=>{
         
-        console.log(e.target.textContent) ;
         if(num1){
                 num1 += e.target.textContent ;
                 inp.value += e.target.textContent ;
@@ -18,77 +17,95 @@ document.querySelectorAll('.num').forEach(button => button.addEventListener('cli
         } 
 
         // inp.value += e.target.textContent ;
-        console.log(num1 , inp.value) ;
+        console.log(num1) ;
 }))
 
 // operator  click  handler 
 
 document.querySelectorAll('.operator').forEach(button => button.addEventListener('click' , (e)=>{
-        inp.value = "";
-
-        console.log('operator :' ,e.target.textContent) ;
-        inp.value = e.target.textContent  ;
         
-        prev_op = curr_op ;
-        curr_op = e.target.textContent ;
+       
+        console.log('operator :' , e.target.textContent) ;
+        inp.value = e.target.textContent  ;
+        if(prev_op != null){
+               op_handler(prev_op);
+        }
+        if(prev_op === null){
+                prev_op = e.target.textContent ;
+                curr_op = e.target.textContent ;
+                total = num1 ;
+                num1 = null ;
+        }
+        else{
+                prev_op = curr_op ;
+                curr_op = e.target.textContent ;
+        }
 
-        switch (curr_op) {
+        
+        
+        
+}))
+
+
+function op_handler(prev_op){
+        switch (prev_op) {
                 case "+":
-                  
-                  curr_op = '+' ;
-                  add(num1);  
+                  total += Number(num1) ;
+                  num1 = null ; 
                         break;
                 
                 case "-":
-                   sub(num1);  
+                  total -= Number(num1) ;
+                  num1 = null ;
+                    
                         break;
 
                 case "*":
+                  
+                  total *= Number(num1) ;
+                  num1 = null ;
                    mul(num1);   
                         break;
 
                 case "/":
-                   div(num1);    
+                   total /= Number(num1) ;
+                  num1 = null ;    
                         break;
                 
                 case "C":
-                    clear(num1);   
+                    num1 = null ;   
                         break;
 
                 case "=":
-                        
+                        console.log(total) ;
                         break;
 
-                default:
-                        break;
         }
-}))
-
-
-function op_handler(total ,  num1 , op){
-
         
 }
 
-function add(num1) {
-        if(total == 0 && prev_operator == null) total =  num1 ;
-        else total += num1 ;
-        inp.value =  total ;
-}
+// function add(num1) {
+//         if(prev_op != null){
+//                 total
+//         }
+//         if(total == 0 && prev_operator == null) total =  num1 ;
+//         else total += num1 ;
+//         inp.value =  total ;
+// }
 
-function sub(num1) {
-        if(total == 0 ) total =  num1 ;
-        else total -= num1 ;
-}
+// function sub(num1) {
+//         if(total == 0 ) total =  num1 ;
+//         else total -= num1 ;
+// }
 
-function mul(num1) {
-        if(total == 0 ) total =  num1 ;
-        else total *= num1 ;
+// function mul(num1) {
+//         if(total == 0 ) total =  num1 ;
+//         else total *= num1 ;
 
-}
+// }
 
-function div(num1) {
-        if(total == 0 ) total =  num1 ;
-        else total /= num1 ;
-}
+// function div(num1) {
+//         if(total == 0 ) total =  num1 ;
+//         else total /= num1 ;
+// }
 
